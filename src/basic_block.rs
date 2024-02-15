53d900f76f97423125327b7fe17086e3904f15e6
Bing-Jyue Chen <chenbingjyue@gmail.com>
2024-02-15 11:06:22 -0600

    patch some compiling issues

--- src/basic_block.rs
+++ src/basic_block.rs
@@ -1,73 +1,67 @@
-#![allow(unused_variables)]
-use crate::util;
-pub use add::AddBasicBlock;
-use ark_bn254::{Fr, G1Affine, G1Projective, G2Affine};
 use ark_poly::univariate::DensePolynomial;
-use ark_poly::{EvaluationDomain, GeneralEvaluationDomain};
+use ark_poly::{GeneralEvaluationDomain, EvaluationDomain};
+use ark_bn254::{Fr, G1Affine, G1Projective, G2Affine};
+use rand::Rng;
+use ndarray::{Array, IxDyn};
+use crate::util;
 pub use cq::CQBasicBlock;
 pub use cqlin::CQLinBasicBlock;
 pub use mul::MulBasicBlock;
-use rand::Rng;
-pub mod add;
+pub use add::AddBasicBlock;
 pub mod cq;
 pub mod cqlin;
 pub mod mul;
+pub mod add;
 
-pub struct Data {
-  pub raw: Vec<Fr>,
+pub struct Data{
+  pub raw : Tensor<Fr>,
   pub poly: DensePolynomial<Fr>,
-  pub g1: G1Affine,
+  pub g1: G1Affine
 }
-impl Data {
-  pub fn new(srs: (&Vec<G1Affine>, &Vec<G2Affine>), raw: &Vec<Fr>) -> Data {
-    let N = (*raw).len();
-    let domain = GeneralEvaluationDomain::<Fr>::new(N).unwrap();
-    let f = DensePolynomial { coeffs: domain.ifft(raw) };
+impl Data{
+  pub fn new(srs:(&Vec<G1Affine>,&Vec<G2Affine>), raw:&Tensor<Fr>) -> Data{
+    let raw_vec = raw.clone().into_raw_vec();
+    let N = (*raw_vec).len();
+    let domain  = GeneralEvaluationDomain::<Fr>::new(N).unwrap();
+    let f = DensePolynomial{coeffs: domain.ifft(&raw_vec)};
     let fx: G1Affine = util::msm::<G1Projective>(&srs.0[..N], &f.coeffs).into();
-    return Data {
-      raw: raw.to_vec(),
-      poly: f,
-      g1: fx,
-    };
+    return Data{raw: raw.clone(), poly: f, g1: fx};
   }
 }
-pub struct DataEnc {
-  pub len: usize,
+pub struct DataEnc{
+  pub len : usize,
   pub g1: G1Affine,
+  pub shape: Vec<usize>,
 }
-impl DataEnc {
-  pub fn new(data: &Data) -> DataEnc {
-    return DataEnc {
-      len: data.raw.len(),
-      g1: data.g1,
-    };
+impl DataEnc{
+  pub fn new(data:&Data) -> DataEnc{
+    return DataEnc{len: data.raw.len(), g1: data.g1, shape: data.raw.shape().to_owned()};
   }
 }
-pub trait BasicBlock {
-  fn run(model: &Vec<Fr>, inputs: &Vec<Vec<Fr>>) -> Vec<Fr> {
-    Vec::new()
-  }
-  fn setup(srs: (&Vec<G1Affine>, &Vec<G2Affine>), model: &Data) -> (Vec<G1Affine>, Vec<G2Affine>) {
-    (Vec::new(), Vec::new())
-  }
-  fn prove<R: Rng>(
-    srs: (&Vec<G1Affine>, &Vec<G2Affine>),
-    setup: (&Vec<G1Affine>, &Vec<G2Affine>),
-    model: &Data,
-    inputs: &Vec<Data>,
-    output: &Data,
-    rng: &mut R,
-  ) -> (Vec<G1Affine>, Vec<G2Affine>) {
-    (Vec::new(), Vec::new())
-  }
-  fn verify<R: Rng>(
-    srs: (&Vec<G1Affine>, &Vec<G2Affine>),
-    model: &DataEnc,
-    inputs: &Vec<DataEnc>,
-    output: &DataEnc,
-    proof: (&Vec<G1Affine>, &Vec<G2Affine>),
-    rng: &mut R,
-  ) {
-    ()
-  }
+
+pub type Tensor<F> = Array<F, IxDyn>;
+pub trait BasicBlock{
+  type Setup;
+  type Proof;
+  fn run(model: &Vec<Tensor<Fr>>,
+         inputs: &Vec<Tensor<Fr>>) ->
+         Vec<Tensor<Fr>>;
+  fn setup(srs: (&Vec<G1Affine>,&Vec<G2Affine>),
+           model: &Data) -> Self::Setup;
+          //(Vec<G1Affine>,Vec<G2Affine>);
+  fn prove<R: Rng>(srs: (&Vec<G1Affine>,&Vec<G2Affine>),
+                   setup: &Self::Setup,
+                   model: &Data,
+                   inputs: &Vec<Data>,
+                   output: &Data,
+                   rng: &mut R) -> Self::Proof;
+                  //(Vec<G1Affine>,Vec<G2Affine>,Vec<Fr>);
+  fn verify<R: Rng>(srs: (&Vec<G1Affine>,&Vec<G2Affine>),
+                    model: &DataEnc,
+                    inputs: &Vec<DataEnc>,
+                    output: &DataEnc,
+                    proof: &Self::Proof,
+                    rng: &mut R);
 }
+
+
