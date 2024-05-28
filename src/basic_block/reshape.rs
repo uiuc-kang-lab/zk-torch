2a6f056db919f499b979628c9934e8e259e3df51
Lilia Tang <liliatangxy@gmail.com>
2024-05-28 11:13:22 -0700

    spacing fix

--- src/basic_block/reshape.rs
+++ src/basic_block/reshape.rs
@@ -7,16 +7,19 @@ use rand::rngs::StdRng;
 pub struct ReshapeBasicBlock {
   pub shape: Vec<usize>,
 }
+
 impl BasicBlock for ReshapeBasicBlock {
   fn run(&self, _model: &ArrayD<Fr>, inputs: &Vec<&ArrayD<Fr>>) -> Vec<ArrayD<Fr>> {
     assert!(inputs.len() == 1);
     assert!(inputs[0].shape().last() == self.shape.last());
     vec![inputs[0].view().into_shape(&self.shape[..]).unwrap().to_owned()]
   }
+
   fn encodeOutputs(&self, _srs: &SRS, _model: &ArrayD<Data>, inputs: &Vec<&ArrayD<Data>>, _outputs: &Vec<&ArrayD<Fr>>) -> Vec<ArrayD<Data>> {
     let n = self.shape.len();
     vec![inputs[0].view().into_shape(&self.shape[..n - 1]).unwrap().to_owned()]
   }
+
   fn verify(
     &self,
     _srs: &SRS,
