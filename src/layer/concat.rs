c5e1ad8a38265335af8c2bfd178ee38b8ae14882
Bing-Jyue Chen <chenbingjyue@gmail.com>
2024-06-09 20:12:21 -0500

    address comments for concat

--- src/layer/concat.rs
+++ src/layer/concat.rs
@@ -3,8 +3,27 @@ use crate::graph::*;
 use crate::layer::Layer;
 use crate::util;
 use ark_bn254::Fr;
-use ndarray::ArrayD;
+use ndarray::{ArrayD, IxDyn};
 use tract_onnx::pb::AttributeProto;
+
+fn get_concat_indices(input_shapes:  &Vec<&Vec<usize>>, output_shape: &Vec<usize>, axis: usize) -> Vec<ArrayD<Option<IxDyn>>> {
+  let mut indices = vec![];
+  let mut axis_offset = 0;
+  for i in 0..input_shapes.len() {
+    let output = ArrayD::from_shape_fn(output_shape.as_slice(), |index| {
+      if index[axis] >= axis_offset && index[axis] < axis_offset + input_shapes[i][axis] {
+        let mut new_index = index.clone();
+        new_index[axis] = index[axis] - axis_offset;
+        Some(new_index)
+      } else {
+        None
+      }
+    });
+    axis_offset += input_shapes[i][axis];
+    indices.push(output);
+  }
+  indices
+}
 
 pub struct ConcatLayer;
 impl Layer for ConcatLayer {
@@ -15,41 +34,35 @@ impl Layer for ConcatLayer {
     let axis = (if axis < 0 { input_shapes[0].len() as isize + axis } else { axis }) as usize;
     let mut outputShape = input_shapes[0].clone();
     outputShape[axis] = input_shapes.iter().map(|x| x[axis as usize]).sum();
-    if input_shapes[0].len() == 1 {
-      // workaround: for 1D input. Maybe we should use copy constraint to handle this case later.
-      let n_input = input_shapes.len();
-      let concat = graph.addBB(Box::new(ConcatBasicBlock { axis: axis as usize }));
-      let concat_output = graph.addNode(concat, (0..n_input).map(|i| (-(i as i32 + 1), 0)).collect());
-      graph.outputs.push((concat_output, 0));
-    } else if axis == input_shapes[0].len() - 1 {
-      // permute inputs
-      let n = outputShape.len();
-      let mut a = outputShape[n - 2];
-      let mut b = outputShape[n - 1];
-      (a, b) = (util::next_pow(a as u32) as usize, util::next_pow(b as u32) as usize);
-      let permutation = (vec![0], (0..a).map(|x| x).collect());
-      let permute = graph.addBB(Box::new(RepeaterBasicBlock {
-        basic_block: Box::new(PermuteBasicBlock { permutation: permutation }),
-        N: 2,
-      }));
-      let concat = graph.addBB(Box::new(ConcatBasicBlock { axis: (axis - 1) as usize }));
-      let permutation_back = ((0..a).map(|x| x * b).collect(), (0..b).collect());
-      let permute_back = graph.addBB(Box::new(RepeaterBasicBlock {
-        basic_block: Box::new(PermuteBasicBlock {
-          permutation: permutation_back,
-        }),
-        N: 2,
-      }));
-
-      let n_input = input_shapes.len();
-      let mut n_permute_output = Vec::with_capacity(n_input);
-      for i in 0..n_input {
-        let permute_output = graph.addNode(permute, vec![(-(i as i32 + 1), 0)]);
-        n_permute_output.push((permute_output, 0));
+    if axis == input_shapes[0].len() - 1 {
+      let mut padded_output_shape = outputShape.clone();
+      padded_output_shape[axis] = util::next_pow(padded_output_shape[axis] as u32) as usize;
+      let permutations = get_concat_indices(input_shapes, &padded_output_shape, axis);
+      let mut cc_basicblocks = vec![];
+      for i in 0..input_shapes.len() {
+        let cc = graph.addBB(Box::new(CopyConstraintBasicBlock {
+          permutation: permutations[i].clone(),
+          input_dim: IxDyn(&input_shapes[i]),
+        }));
+        cc_basicblocks.push(cc);
       }
-      let concat_output = graph.addNode(concat, n_permute_output);
-      let output = graph.addNode(permute_back, vec![(concat_output, 0)]);
-      graph.outputs.push((output, 0));
+      let add = graph.addBB(Box::new(RepeaterBasicBlock {
+        basic_block: Box::new(AddBasicBlock {}),
+        N: 1,
+      }));
+      
+      let mut cc_outputs = vec![];
+      for i in 0..input_shapes.len() {
+        let cc_output = graph.addNode(cc_basicblocks[i], vec![(-(i as i32 + 1), 0)]);
+        cc_outputs.push((cc_output, 0));
+      }
+      // add 2 cc_outputs and reduce to 1 output until only 1 output left
+      while cc_outputs.len() > 1 {
+        let add_output = graph.addNode(add, vec![cc_outputs.pop().unwrap(), cc_outputs.pop().unwrap()]);
+        cc_outputs.push((add_output, 0));
+      }
+      let final_output = cc_outputs.pop().unwrap();
+      graph.outputs.push(final_output);
     } else {
       let n_input = input_shapes.len();
       let concat = graph.addBB(Box::new(ConcatBasicBlock { axis: axis as usize }));
