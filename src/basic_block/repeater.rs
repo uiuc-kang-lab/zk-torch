52392e12531a6874c1633af6ccb945a2a38bbf4f
Bing-Jyue Chen <chenbingjyue@gmail.com>
2024-07-15 16:37:57 -0500

    cleanup

--- src/basic_block/repeater.rs
+++ src/basic_block/repeater.rs
@@ -7,108 +7,6 @@ use ndarray::{arr1, azip, par_azip, s, ArrayD, Axis, Dimension, IxDyn, SliceInfo
 use rand::rngs::StdRng;
 use rayon::prelude::*;
 
-fn repeater_run_wrapper(model: &ArrayD<Fr>, inputs: &Vec<&ArrayD<Fr>>, basic_block: &dyn BasicBlock, N: usize) -> Vec<ArrayD<Fr>> {
-  let temp = broadcastN::<Fr, Fr>(inputs, None, N);
-  let temp = temp.map(|(subArrays, _)| {
-    let subArrays: Vec<_> = util::vec_iter(subArrays).map(|y| y).collect();
-    basic_block.run(model, &subArrays)
-  });
-  let temp = temp.map(|x| x.iter().map(|y| y).collect());
-  let temp = temp.map(|x| x);
-  combineArr(&temp)
-}
-
-fn repeater_encodeOutputs_wrapper(
-  srs: &SRS,
-  model: &ArrayD<Data>,
-  inputs: &Vec<&ArrayD<Data>>,
-  outputs: &Vec<&ArrayD<Fr>>,
-  basic_block: &dyn BasicBlock,
-  N: usize,
-) -> Vec<ArrayD<Data>> {
-  let mut temp = broadcastN(inputs, Some(outputs), N - 1);
-  let mut empty = ArrayD::from_elem(temp.shape(), vec![]);
-  ndarr_azip!(((localInputs, localOutputs) in &mut temp, x in &mut empty) {
-    let localInputs: Vec<_> = localInputs.iter().map(|y| y).collect();
-    let localOutputs: Vec<_> = localOutputs.as_ref().unwrap().iter().map(|y| y).collect();
-    *x = basic_block.encodeOutputs(srs, model, &localInputs, &localOutputs);
-  });
-  let temp = empty.map(|x| x.iter().map(|y| y).collect());
-  let temp = temp.map(|x| x);
-  combineArr(&temp)
-}
-
-fn repeater_prove_wrapper(
-  srs: &SRS,
-  setup: (&Vec<G1Affine>, &Vec<G2Affine>, &Vec<DensePolynomial<Fr>>),
-  model: &ArrayD<Data>,
-  inputs: &Vec<&ArrayD<Data>>,
-  outputs: &Vec<&ArrayD<Data>>,
-  rng: &mut StdRng,
-  cache: ProveVerifyCache,
-  basic_block: &dyn BasicBlock,
-  N: usize,
-) -> (Vec<G1Projective>, Vec<G2Projective>, Vec<Fr>) {
-  let mut temp = broadcastN(inputs, Some(outputs), N - 1);
-  let mut empty = ArrayD::from_elem(temp.shape(), (vec![], vec![], vec![]));
-  ndarr_azip!(((localInputs, localOutputs) in &mut temp, x in &mut empty) {
-    let localInputs: Vec<_> = localInputs.iter().map(|y| y).collect();
-    let localOutputs: Vec<_> = localOutputs.as_ref().unwrap().iter().map(|y| y).collect();
-    let mut rng = rng.clone();
-    let tmp = basic_block.prove(srs, setup, model, &localInputs, &localOutputs, &mut rng, cache.clone());
-    *x = tmp;
-  });
-  let proof: (Vec<_>, Vec<_>, Vec<_>) = multiunzip(empty.into_iter());
-  let proof = (
-    proof.0.into_iter().flatten().collect(),
-    proof.1.into_iter().flatten().collect(),
-    proof.2.into_iter().flatten().collect(),
-  );
-  proof
-}
-
-fn repeater_verify_wrapper(
-  srs: &SRS,
-  model: &ArrayD<DataEnc>,
-  inputs: &Vec<&ArrayD<DataEnc>>,
-  outputs: &Vec<&ArrayD<DataEnc>>,
-  proof: (&Vec<G1Affine>, &Vec<G2Affine>, &Vec<Fr>),
-  rng: &mut StdRng,
-  cache: ProveVerifyCache,
-  basic_block: &dyn BasicBlock,
-  N: usize,
-) -> Vec<PairingCheck> {
-  let mut temp = broadcastN(inputs, Some(outputs), N - 1);
-
-  let l = temp.len();
-  let divA = proof.0.len() / l;
-  let divB = proof.1.len() / l;
-  let divC = proof.2.len() / l;
-  let combined: Vec<_> = (0..l)
-    .map(|i| {
-      (
-        &proof.0[i * divA..i * divA + divA],
-        &proof.1[i * divB..i * divB + divB],
-        &proof.2[i * divC..i * divC + divC],
-      )
-    })
-    .collect();
-  let mut proofArr = ArrayD::from_shape_vec(temp.shape(), combined).unwrap();
-
-  let mut empty = ArrayD::from_elem(temp.shape(), vec![]);
-  ndarr_azip!(((localInputs, localOutputs) in &mut temp, localProof in &mut proofArr, x in &mut empty){
-    let localInputs: Vec<_> = localInputs.iter().map(|y| y).collect();
-    let localOutputs: Vec<_> = localOutputs.as_ref().unwrap().iter().map(|y| y).collect();
-    let localProof = (&localProof.0.to_vec(), &localProof.1.to_vec(), &localProof.2.to_vec());
-    let mut rng = rng.clone();
-    let temp = basic_block.verify(srs, model, &localInputs, &localOutputs, localProof, &mut rng, cache.clone());
-    *x = temp;
-  });
-  let pairings = empty.into_iter().flatten().collect();
-
-  pairings
-}
-
 #[derive(Debug)]
 pub struct RepeaterBasicBlock {
   pub basic_block: Box<dyn BasicBlock>,
@@ -204,11 +102,27 @@ impl BasicBlock for RepeaterBasicBlock {
   }
 
   fn run(&self, model: &ArrayD<Fr>, inputs: &Vec<&ArrayD<Fr>>) -> Vec<ArrayD<Fr>> {
-    repeater_run_wrapper(model, inputs, &*self.basic_block, self.N)
+    let temp = broadcastN::<Fr, Fr>(inputs, None, self.N);
+    let temp = temp.map(|(subArrays, _)| {
+      let subArrays: Vec<_> = util::vec_iter(subArrays).map(|y| y).collect();
+      self.basic_block.run(model, &subArrays)
+    });
+    let temp = temp.map(|x| x.iter().map(|y| y).collect());
+    let temp = temp.map(|x| x);
+    combineArr(&temp)
   }
 
   fn encodeOutputs(&self, srs: &SRS, model: &ArrayD<Data>, inputs: &Vec<&ArrayD<Data>>, outputs: &Vec<&ArrayD<Fr>>) -> Vec<ArrayD<Data>> {
-    repeater_encodeOutputs_wrapper(srs, model, inputs, outputs, &*self.basic_block, self.N)
+    let mut temp = broadcastN(inputs, Some(outputs), self.N - 1);
+    let mut empty = ArrayD::from_elem(temp.shape(), vec![]);
+    ndarr_azip!(((localInputs, localOutputs) in &mut temp, x in &mut empty) {
+      let localInputs: Vec<_> = localInputs.iter().map(|y| y).collect();
+      let localOutputs: Vec<_> = localOutputs.as_ref().unwrap().iter().map(|y| y).collect();
+      *x = self.basic_block.encodeOutputs(srs, model, &localInputs, &localOutputs);
+    });
+    let temp = empty.map(|x| x.iter().map(|y| y).collect());
+    let temp = temp.map(|x| x);
+    combineArr(&temp)
   }
 
   fn setup(&self, srs: &SRS, model: &ArrayD<Data>) -> (Vec<G1Projective>, Vec<G2Projective>, Vec<DensePolynomial<Fr>>) {
@@ -225,7 +139,22 @@ impl BasicBlock for RepeaterBasicBlock {
     rng: &mut StdRng,
     cache: ProveVerifyCache,
   ) -> (Vec<G1Projective>, Vec<G2Projective>, Vec<Fr>) {
-    repeater_prove_wrapper(srs, setup, model, inputs, outputs, rng, cache, &*self.basic_block, self.N)
+    let mut temp = broadcastN(inputs, Some(outputs), self.N - 1);
+    let mut empty = ArrayD::from_elem(temp.shape(), (vec![], vec![], vec![]));
+    ndarr_azip!(((localInputs, localOutputs) in &mut temp, x in &mut empty) {
+      let localInputs: Vec<_> = localInputs.iter().map(|y| y).collect();
+      let localOutputs: Vec<_> = localOutputs.as_ref().unwrap().iter().map(|y| y).collect();
+      let mut rng = rng.clone();
+      let tmp = self.basic_block.prove(srs, setup, model, &localInputs, &localOutputs, &mut rng, cache.clone());
+      *x = tmp;
+    });
+    let proof: (Vec<_>, Vec<_>, Vec<_>) = multiunzip(empty.into_iter());
+    let proof = (
+      proof.0.into_iter().flatten().collect(),
+      proof.1.into_iter().flatten().collect(),
+      proof.2.into_iter().flatten().collect(),
+    );
+    proof
   }
 
   fn verify(
@@ -238,6 +167,34 @@ impl BasicBlock for RepeaterBasicBlock {
     rng: &mut StdRng,
     cache: ProveVerifyCache,
   ) -> Vec<PairingCheck> {
-    repeater_verify_wrapper(srs, model, inputs, outputs, proof, rng, cache, &*self.basic_block, self.N)
+    let mut temp = broadcastN(inputs, Some(outputs), self.N - 1);
+
+    let l = temp.len();
+    let divA = proof.0.len() / l;
+    let divB = proof.1.len() / l;
+    let divC = proof.2.len() / l;
+    let combined: Vec<_> = (0..l)
+      .map(|i| {
+        (
+          &proof.0[i * divA..i * divA + divA],
+          &proof.1[i * divB..i * divB + divB],
+          &proof.2[i * divC..i * divC + divC],
+        )
+      })
+      .collect();
+    let mut proofArr = ArrayD::from_shape_vec(temp.shape(), combined).unwrap();
+
+    let mut empty = ArrayD::from_elem(temp.shape(), vec![]);
+    ndarr_azip!(((localInputs, localOutputs) in &mut temp, localProof in &mut proofArr, x in &mut empty){
+      let localInputs: Vec<_> = localInputs.iter().map(|y| y).collect();
+      let localOutputs: Vec<_> = localOutputs.as_ref().unwrap().iter().map(|y| y).collect();
+      let localProof = (&localProof.0.to_vec(), &localProof.1.to_vec(), &localProof.2.to_vec());
+      let mut rng = rng.clone();
+      let temp = self.basic_block.verify(srs, model, &localInputs, &localOutputs, localProof, &mut rng, cache.clone());
+      *x = temp;
+    });
+    let pairings = empty.into_iter().flatten().collect();
+
+    pairings
   }
 }
