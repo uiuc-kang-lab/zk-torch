0e34a37488da285a8f0051c36cecc1ea68a7a6e4
Bing-Jyue Chen <chenbingjyue@gmail.com>
2024-07-27 12:41:13 -0500

    add bool_check

--- src/layer/not.rs
+++ src/layer/not.rs
@@ -12,6 +12,7 @@ pub struct NotLayer;
 impl Layer for NotLayer {
   fn graph(input_shapes: &Vec<&Vec<usize>>, _constants: &Vec<Option<&ArrayD<Fr>>>, _attributes: &Vec<&AttributeProto>) -> (Graph, Vec<Vec<usize>>) {
     let mut graph = Graph::new();
+    let bool_check = graph.addBB(Box::new(BooleanCheckBasicBlock {}));
     let one = graph.addBB(Box::new(Const2BasicBlock {
       c: arr1(&vec![Fr::one(); *input_shapes[0].last().unwrap()]).into_dyn(),
     }));
@@ -19,6 +20,7 @@ impl Layer for NotLayer {
       basic_block: Box::new(SubBasicBlock {}),
       N: 1,
     }));
+    let _ = graph.addNode(bool_check, vec![(-1, 0)]);
     let one_output = graph.addNode(one, vec![]);
     let layer_output = graph.addNode(layer, vec![(one_output, 0), (-1, 0)]);
     graph.outputs.push((layer_output, 0));
