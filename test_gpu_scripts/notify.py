8f4924dd964292c9d7ccac0779688099e6e11ba7
Bing-Jyue Chen <chenbingjyue@gmail.com>
2024-07-30 18:49:46 -0500

    remove -k

--- test_gpu_scripts/notify.py
+++ test_gpu_scripts/notify.py
@@ -4,18 +4,25 @@ import sys
 pr_number = sys.argv[1]
 token = sys.argv[2]
 
-message = 'PR #' + str(pr_number) + 'successfully passed the gpu test'
+message = 'PR #' + str(pr_number) + ' successfully passed the gpu test'
 with open("zktorch_gh_action.out", "r") as f:
   contents = f.readlines()
 
-start_scan = False
-for c in contents:
-  if 'Running `target/debug/zkml_proofs`' in c:
-    start_scan = True
-  if start_scan and 'error:' in c or "thread 'main' panicked" in c:
-    message = 'error found in the gpu test in PR #' + str(pr_number)
-    break
+message = 'error found in the gpu test in PR #' + str(pr_number)
 
+# Check if log file exists
+try:
+    with open("zktorch_gh_action.out", "r") as f:
+      contents = f.readlines()
+    # Check if the log file contains the string Cargo run was successful.
+    for line in contents:
+      if "Cargo run was successful." in line:
+        message = 'PR #' + str(pr_number) + ' successfully passed the gpu test'
+        break
+except FileNotFoundError:
+    message = 'error found in the gpu test in PR #' + str(pr_number)
+
+# reference: https://www.datacamp.com/tutorial/how-to-send-slack-messages-with-python
 # Set up a WebClient with the Slack OAuth token
 client = WebClient(token=token)
 
