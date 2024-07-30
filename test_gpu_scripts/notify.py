80ea58eb171f3ad0680acfb67c5b30717e7b9642
Bing-Jyue Chen <chenbingjyue@gmail.com>
2024-07-30 16:33:51 -0500

    add slack message

---
+++ test_gpu_scripts/notify.py
@@ -0,0 +1,27 @@
+from slack_sdk import WebClient
+import sys 
+
+pr_number = sys.argv[1]
+token = sys.argv[2]
+
+message = 'PR #' + str(pr_number) + 'successfully passed the gpu test'
+with open("zktorch_gh_action.out", "r") as f:
+  contents = f.readlines()
+
+start_scan = False
+for c in contents:
+  if 'Running `target/debug/zkml_proofs`' in c:
+    start_scan = True
+  if start_scan and 'error:' in c or "thread 'main' panicked" in c:
+    message = 'error found in the gpu test in PR #' + str(pr_number)
+    break
+
+# Set up a WebClient with the Slack OAuth token
+client = WebClient(token=token)
+
+# Send a message
+client.chat_postMessage(
+    channel="bot-updates", 
+    text=message, 
+    username="SLURM bot"
+)
