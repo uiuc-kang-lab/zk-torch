95ef4ad7cc4d7c8539d7fbb1b81d5d3ba0463a2c
Bing-Jyue Chen <chenbingjyue@gmail.com>
2024-08-29 18:02:41 -0500

    gpt shape issues

--- src/layer/pow.rs
+++ src/layer/pow.rs
@@ -2,10 +2,34 @@ use crate::basic_block::*;
 use crate::graph::*;
 use crate::layer::Layer;
 use crate::onnx;
+use crate::util;
 use ark_bn254::Fr;
+use ark_std::One;
 use ndarray::ArrayD;
 use tract_onnx::pb::AttributeProto;
 use tract_onnx::prelude::DatumType;
+
+#[derive(Debug)]
+pub struct PrecomputedPowBasicBlock {
+  pub input_SF: usize,
+  pub output_SF: usize,
+}
+impl BasicBlock for PrecomputedPowBasicBlock {
+  fn run(&self, _model: &ArrayD<Fr>, inputs: &Vec<&ArrayD<Fr>>) -> Vec<ArrayD<Fr>> {
+    let base = util::fr_to_int(*inputs[0].first().unwrap());
+    let shape = inputs[1].shape();
+    let out = util::array_into_iter(inputs[1])
+      .map(|x| {
+        let mut x = util::fr_to_int(*x) as f32;
+        x /= (1 << self.input_SF) as f32;
+        x = x.powi(base);
+        x *= (1 << self.output_SF) as f32;
+        Fr::from(x.round() as i32)
+      })
+      .collect::<Vec<_>>();
+    vec![ArrayD::from_shape_vec(shape, out).unwrap()]
+  }
+}
 
 pub struct PowLayer;
 impl Layer for PowLayer {
@@ -16,7 +40,34 @@ impl Layer for PowLayer {
     _attributes: &Vec<&AttributeProto>,
   ) -> (Graph, Vec<Vec<usize>>, Vec<DatumType>) {
     let mut graph = Graph::new();
-    assert!(constants[1].unwrap().0.first().unwrap() == &Fr::from(2 * *onnx::SF as u32));
+
+    // if both constants[0] and constants[1] are Some
+    if constants[0].is_some() && constants[1].is_some() {
+      // if constants[0].len() == 1 and constants[1].len() > 1
+      if constants[0].unwrap().0.len() == 1 && constants[1].unwrap().0.len() > 1 {
+        let pow = graph.addBB(Box::new(PrecomputedPowBasicBlock {
+          input_SF: *onnx::SF_LOG,
+          output_SF: *onnx::SF_LOG,
+        }));
+        let pow_output = graph.addNode(pow, vec![(-1, 0), (-2, 0)]);
+        graph.outputs.push((pow_output, 0));
+        return (graph, vec![input_shapes[1].clone()], vec![input_types[0]]);
+      }
+    }
+    assert!(constants[1].unwrap().0.len() == 1);
+    let N = util::fr_to_int(*constants[1].unwrap().0.first().unwrap());
+    assert!(N >= 0);
+    if N == 0 {
+      let endShape_padded: Vec<usize> = input_shapes[0].clone().iter().map(|&x| util::next_pow(x as u32) as usize).collect();
+      let one = graph.addBB(Box::new(ConstOfShapeBasicBlock {
+        c: Fr::one(),
+        shape: endShape_padded.clone(),
+      }));
+      let one_output = graph.addNode(one, vec![]);
+      graph.outputs.push((one_output, 0));
+      return (graph, vec![input_shapes[0].clone()], vec![input_types[0]]);
+    }
+
     let mul = graph.addBB(Box::new(RepeaterBasicBlock {
       basic_block: Box::new(MulBasicBlock {}),
       N: 1,
@@ -38,9 +89,14 @@ impl Layer for PowLayer {
       }),
       N: 1,
     }));
-    let mul_output = graph.addNode(mul, vec![(-1, 0), (-1, 0)]);
-    let change_SF_output = graph.addNode(change_SF, vec![(mul_output, 0)]);
-    let _ = graph.addNode(change_SF_check, vec![(mul_output, 0), (change_SF_output, 0)]);
+    let mut mul_output = -1;
+    let mut change_SF_output = -1;
+    for _i in 1..N {
+      mul_output = graph.addNode(mul, vec![(-1, 0), (mul_output, 0)]);
+      change_SF_output = graph.addNode(change_SF, vec![(mul_output, 0)]);
+      let _ = graph.addNode(change_SF_check, vec![(mul_output, 0), (change_SF_output, 0)]);
+    }
+
     graph.outputs.push((change_SF_output, 0));
     (graph, vec![input_shapes[0].clone()], vec![input_types[0]])
   }
