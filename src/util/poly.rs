c50323f007c48683143f9cf91ee11ecb2951733d
Bing-Jyue Chen <bjchen4@illinois.edu>
2024-09-02 00:49:07 -0500

    Accelerate poly multiplication (#114)
    * accelerate poly mul
    
    * remove unnecessary lib
    
    * add comments

--- src/util/poly.rs
+++ src/util/poly.rs
@@ -11,22 +11,44 @@ where
   // Assuming vecs is non-empty and all vectors have the same length
   let m = vecs[0].len();
 
-  (0..m).into_par_iter().map(|i| vecs.iter().map(|v| v[i]).product()).collect()
+  (0..m).into_par_iter().map(|i| vecs[0][i] * vecs[1][i]).collect()
 }
 
-pub fn mul_polys(polys: &Vec<DensePolynomial<Fr>>) -> DensePolynomial<Fr> {
-  let N: usize = polys.iter().map(|p| p.coeffs.len()).sum();
-  let domain = GeneralEvaluationDomain::<Fr>::new(N).unwrap();
-  if polys[0].is_zero() {
+pub fn mul_two_polys(polys: &Vec<DensePolynomial<Fr>>) -> DensePolynomial<Fr> {
+  assert!(polys.len() == 2);
+  if polys[0].is_zero() || polys[1].is_zero() {
     return DensePolynomial::zero();
   }
-  let mut p_evals = domain.fft(&polys[0].coeffs);
-  for p in polys[1..].iter() {
-    if p.is_zero() {
-      return DensePolynomial::zero();
-    } else {
-      p_evals = elementwise_product(&vec![p_evals, domain.fft(&p.coeffs)]);
-    }
-  }
+  let N: usize = polys.iter().map(|p| p.coeffs.len()).sum();
+  let domain = GeneralEvaluationDomain::<Fr>::new(N).unwrap();
+
+  let p_evals = polys.par_iter().map(|p| domain.fft(&p.coeffs)).collect::<Vec<_>>();
+  let p_evals = elementwise_product(&p_evals);
   DensePolynomial::from_coefficients_vec(domain.ifft(&p_evals))
+}
+
+// Multiply a list of polynomials in parallel
+// TODO: explore if there exists a more efficient parallel algorithm
+pub fn mul_polys(polys: &Vec<DensePolynomial<Fr>>) -> DensePolynomial<Fr> {
+  // Base case: if the list has only one polynomial, return it directly
+  if polys.len() == 1 {
+    return polys[0].clone();
+  }
+
+  // Parallel recursive case: pairwise multiply the polynomials
+  let next_level: Vec<DensePolynomial<Fr>> = polys
+    .par_chunks(2) // Parallelize processing in chunks of 2
+    .map(|chunk| {
+      if chunk.len() == 2 {
+        // If there are two polynomials in the chunk, multiply them
+        mul_two_polys(&vec![chunk[0].clone(), chunk[1].clone()])
+      } else {
+        // If there's only one polynomial in the chunk, return it
+        chunk[0].clone()
+      }
+    })
+    .collect();
+
+  // Recursively call mul_polys on the next level until we get the root
+  mul_polys(&next_level)
 }
