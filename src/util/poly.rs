f74dec5642a0493e61d319782f0eacf27f21568a
Lilia Tang <liliatangxy@gmail.com>
2024-09-29 10:26:44 -0500

    karatsuba

--- src/util/poly.rs
+++ src/util/poly.rs
@@ -20,11 +20,58 @@ pub fn mul_two_polys(polys: &Vec<DensePolynomial<Fr>>) -> DensePolynomial<Fr> {
     return DensePolynomial::zero();
   }
   let N: usize = polys.iter().map(|p| p.coeffs.len()).sum();
-  let domain = GeneralEvaluationDomain::<Fr>::new(N).unwrap();
+  let domain = GeneralEvaluationDomain::<Fr>::new(N);
+  if domain.is_some() {
+    let domain = domain.unwrap();
+    let p_evals = polys.par_iter().map(|p| domain.fft(&p.coeffs)).collect::<Vec<_>>();
+    let p_evals = elementwise_product(&p_evals);
+    DensePolynomial::from_coefficients_vec(domain.ifft(&p_evals))
+  } else {
+    karatsuba_multiply(&polys[0], &polys[1])
+  }
+}
 
-  let p_evals = polys.par_iter().map(|p| domain.fft(&p.coeffs)).collect::<Vec<_>>();
-  let p_evals = elementwise_product(&p_evals);
-  DensePolynomial::from_coefficients_vec(domain.ifft(&p_evals))
+fn mul_by_xn(poly: &DensePolynomial<Fr>, n: usize) -> DensePolynomial<Fr> {
+  let mut new_coeffs = vec![Fr::zero(); n];
+  new_coeffs.extend(poly.coeffs().iter().cloned());
+  DensePolynomial::from_coefficients_vec(new_coeffs)
+}
+
+fn karatsuba_multiply(a: &DensePolynomial<Fr>, b: &DensePolynomial<Fr>) -> DensePolynomial<Fr> {
+  let n = std::cmp::max(a.degree(), b.degree()) + 1;
+
+  // Base case: use standard multiplication for small polynomials
+  if n <= 32 {
+    return a * b;
+  }
+
+  let m = n / 2;
+
+  // Split polynomials
+  let (a0, a1) = split_polynomial(a, m);
+  let (b0, b1) = split_polynomial(b, m);
+
+  // Recursive steps
+  let z0 = karatsuba_multiply(&a0, &b0);
+  let z2 = karatsuba_multiply(&a1, &b1);
+
+  let a0_plus_a1 = &a0 + &a1;
+  let b0_plus_b1 = &b0 + &b1;
+  let z1 = karatsuba_multiply(&a0_plus_a1, &b0_plus_b1);
+
+  // Combine results
+  let mut result = mul_by_xn(&z2, 2 * m);
+  result += &z0;
+  result = &result + &mul_by_xn(&(&(&z1 - &z2) - &z0), m);
+
+  result
+}
+
+fn split_polynomial(p: &DensePolynomial<Fr>, m: usize) -> (DensePolynomial<Fr>, DensePolynomial<Fr>) {
+  let coeffs = p.coeffs();
+  let low = DensePolynomial::from_coefficients_vec(coeffs[..m.min(coeffs.len())].to_vec());
+  let high = DensePolynomial::from_coefficients_vec(coeffs[m.min(coeffs.len())..].to_vec());
+  (low, high)
 }
 
 // Multiply a list of polynomials in parallel
